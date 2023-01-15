4dee2ceeadecacd90b51a75b210e8d00c7f78323
Funny Rose <fannyperretlombard@gmail.com>
2023-01-15 16:40:02 +0100

    Add Navbar Component

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <a href="/">LOGO</a>
+      <ul>
+        <li>
+          <a href="/accueil">Accueil</a>
+        </li>
+        <li>
+          <a href="/domaine-activites">Domaine d'activités</a>
+        </li>
+        <li>
+          <a href="/politique-tarifaire">Politique tarifaire</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
